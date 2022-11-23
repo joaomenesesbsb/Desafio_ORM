cbebb7f2544ff50316fad2eeea4389d388f90295
Joao <joaovic2012@hotmail.com>
2022-11-23 14:19:27 -0300

    Bloco

---
+++ src/main/java/com/devsuperior/desafio/entities/Bloco.java
@@ -0,0 +1,20 @@
+package com.devsuperior.desafio.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_bloco")
+public class Bloco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant inicio;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant fim;
+}
